3e8689f2e6df3743590ae03faec83f41137c995a
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-06 12:07:12 -0800

    created schema for product + created serialize for schema

---
+++ products/models.js
@@ -0,0 +1,49 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const ProductSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: String,
+    required: true
+  },
+  shortDescription: {
+    type: String,
+    required: true
+  },
+  details: {
+    type: String,
+    required: true
+  },
+  style: {
+    type: String,
+    required: true
+  }
+});
+
+ProductSchema.methods.serialize = function() {
+  return {
+    id: this.id,
+    name: this.name || '',
+    imageUrl: this.imageUrl || '',
+    price: this.price || '',
+    shortDescription: this.shortDescription || '',
+    details: this.details || '',
+    style: this.style || ''
+  };
+};
+
+
+const Product = mongoose.model('Product', ProductSchema);
+
+module.exports = {Product};
