2b692b971b6ff0513165830dbcda62c43dd20ffd
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-17 10:42:43 -0700

    sorting products by newest to oldest

--- products/router.js
+++ products/router.js
@@ -1,37 +1,49 @@
-'use strict';
-const express = require('express');
-const bodyParser = require('body-parser');
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const {Product} = require('./models');
+const { Product } = require("./models");
 
 const router = express.Router();
 
 const jsonParser = bodyParser.json();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Product
     .find()
+    .sort({
+      when: 1
+    })
     .then(product => {
       res.json(product.map(product => product.serialize()));
     })
     .catch(err => {
       console.error(err);
-      res.status(500).json({ error: 'something went terribly wrong' });
+      res.status(500).json({ error: "something went terribly wrong" });
     });
 });
 
-router.get('/:id', (req, res) => {
-  Product
-    .findById(req.params.id)
+router.get("/:id", (req, res) => {
+  Product.findById(req.params.id)
     .then(product => res.json(product.serialize()))
     .catch(err => {
       console.error(err);
-      res.status(500).json({ error: 'something went horribly awry' });
+      res.status(500).json({ error: "something went horribly awry" });
     });
 });
 
-router.post('/', jsonParser, (req, res) => {
-  const requiredFields = ['name', 'imageUrl', 'price', 'shortDescription', 'details', 'style','url', 'secondImage', 'thirdImage'];
+router.post("/", jsonParser, (req, res) => {
+  const requiredFields = [
+    "name",
+    "imageUrl",
+    "price",
+    "shortDescription",
+    "details",
+    "style",
+    "url",
+    "secondImage",
+    "thirdImage"
+  ];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -41,71 +53,70 @@ router.post('/', jsonParser, (req, res) => {
     }
   }
 
-  Product
-    .create({
-      name: req.body.name,
-      imageUrl: req.body.imageUrl,
-      price: req.body.price,
-      shortDescription: req.body.shortDescription,
-      details: req.body.details,
-      style: req.body.style,
-      url: req.body.url,
-      secondImage: req.body.secondImage,
-      thirdImage: req.body.thirdImage
-    })
+  Product.create({
+    name: req.body.name,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    shortDescription: req.body.shortDescription,
+    details: req.body.details,
+    style: req.body.style,
+    url: req.body.url,
+    secondImage: req.body.secondImage,
+    thirdImage: req.body.thirdImage
+  })
     .then(product => res.status(201).json(product.serialize()))
     .catch(err => {
       console.error(err);
-      res.status(500).json({ error: 'Something went wrong' });
+      res.status(500).json({ error: "Something went wrong" });
     });
-
 });
 
-
-router.delete('/:id', (req, res) => {
-  Product
-    .findByIdAndRemove(req.params.id)
+router.delete("/:id", (req, res) => {
+  Product.findByIdAndRemove(req.params.id)
     .then(() => {
-      res.status(204).json({ message: 'success' });
+      res.status(204).json({ message: "success" });
     })
     .catch(err => {
       console.error(err);
-      res.status(500).json({ error: 'something went terribly wrong' });
+      res.status(500).json({ error: "something went terribly wrong" });
     });
 });
 
-
-router.put('/:id',jsonParser, (req, res) => {
+router.put("/:id", jsonParser, (req, res) => {
   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     res.status(400).json({
-      error: 'Request path id and request body id values must match'
+      error: "Request path id and request body id values must match"
     });
   }
 
   const updated = {};
-  const updateableFields = ['name', 'imageUrl', 'price', 'shortDescription', 'details', 'style', 'url', 'secondImage', 'thirdImage'];
+  const updateableFields = [
+    "name",
+    "imageUrl",
+    "price",
+    "shortDescription",
+    "details",
+    "style",
+    "url",
+    "secondImage",
+    "thirdImage"
+  ];
   updateableFields.forEach(field => {
     if (field in req.body) {
       updated[field] = req.body[field];
     }
   });
 
-  Product
-    .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+  Product.findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
     .then(updatedProduct => res.status(204).end())
-    .catch(err => res.status(500).json({ message: 'Something went wrong' }));
+    .catch(err => res.status(500).json({ message: "Something went wrong" }));
 });
 
-
-router.delete('/:id', (req, res) => {
-  Product
-    .findByIdAndRemove(req.params.id)
-    .then(() => {
-      console.log(`Deleted blog post with id \`${req.params.id}\``);
-      res.status(204).end();
-    });
+router.delete("/:id", (req, res) => {
+  Product.findByIdAndRemove(req.params.id).then(() => {
+    console.log(`Deleted blog post with id \`${req.params.id}\``);
+    res.status(204).end();
+  });
 });
 
-
-
-module.exports = {router};
+module.exports = { router };
