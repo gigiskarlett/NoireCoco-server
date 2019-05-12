1f3cf13d766d3ed348db716bf35f76c0b260629d
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-05-11 21:57:51 -0700

    added  requiredsignupcode field to router + removed test route to see users

--- users/router.js
+++ users/router.js
@@ -10,7 +10,7 @@ const jsonParser = bodyParser.json();
 
 // Post to register a new user
 router.post("/", jsonParser, (req, res) => {
-  if (signupCode !== "bikini36") {
+  if (signUpCode !== "MimosasByTheSea") {
     return res.status(422).json({
       code: 422,
       reason: "ValidationError",
@@ -19,7 +19,7 @@ router.post("/", jsonParser, (req, res) => {
     });
   }
 
-  const requiredFields = ["username", "password"];
+  const requiredFields = ["username", "password", "signUpCode"];
   const missingField = requiredFields.find(field => !(field in req.body));
 
   if (missingField) {
@@ -31,7 +31,13 @@ router.post("/", jsonParser, (req, res) => {
     });
   }
 
-  const stringFields = ["username", "password", "firstName", "lastName"];
+  const stringFields = [
+    "username",
+    "password",
+    "firstName",
+    "lastName",
+    "signUpCode"
+  ];
   const nonStringField = stringFields.find(
     field => field in req.body && typeof req.body[field] !== "string"
   );
@@ -139,16 +145,6 @@ router.post("/", jsonParser, (req, res) => {
       }
       res.status(500).json({ code: 500, message: "Internal server error" });
     });
-});
-
-// Never expose all your users like below in a prod application
-// we're just doing this so we have a quick way to see
-// if we're creating users. keep in mind, you can also
-// verify this in the Mongo shell.
-router.get("/", (req, res) => {
-  return User.find()
-    .then(users => res.json(users.map(user => user.serialize())))
-    .catch(err => res.status(500).json({ message: "Internal server error" }));
 });
 
 module.exports = { router };
